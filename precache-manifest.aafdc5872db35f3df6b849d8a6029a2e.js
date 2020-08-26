7ba749f9f9fca8b3e06c8c63b73a631d76ac803f
Levitinn <44999446+Levitinn@users.noreply.github.com>
2020-08-26 11:04:26 +0300

    Add files via upload

---
+++ precache-manifest.aafdc5872db35f3df6b849d8a6029a2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f05a95aac4b338c2c5bab6ff6946c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "569d3b76093217b43f7e",
+    "url": "/static/css/main.de58c6fe.chunk.css"
+  },
+  {
+    "revision": "4d79093c2bec3dd012a5",
+    "url": "/static/js/2.db4f5624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db4f5624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569d3b76093217b43f7e",
+    "url": "/static/js/main.cc2eabf8.chunk.js"
+  },
+  {
+    "revision": "be235ca446e18643ca6b",
+    "url": "/static/js/runtime-main.fc250d1f.js"
+  },
+  {
+    "revision": "80c553880f6ab80945b819863d11b56e",
+    "url": "/static/media/avatar.80c55388.jpg"
+  }
+]);
